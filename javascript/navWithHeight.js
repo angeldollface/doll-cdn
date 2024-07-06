e19e2b0cabb270b7a1f2a43f915df477fc5262d9
angeldollface <112771657+angeldollface@users.noreply.github.com>
2024-07-06 10:21:54 +0000

    Deploying to gh-pages from @ angeldollface/doll-cdn@1df2e36ef807a5150b6a2e3372e9da57b79055d1 🚀

---
+++ javascript/navWithHeight.js
@@ -0,0 +1,9 @@
+function openNav(id){
+    let nav = document.getElementById(id);
+    nav.style.width = '100vh';
+}
+
+function closeNav(id){
+    let nav = document.getElementById(id);
+    nav.style.width = '0vh';
+}
---
+++ javascript/navWithWidth.js
@@ -0,0 +1,9 @@
+function openNav(id){
+    let nav = document.getElementById(id);
+    nav.style.width = '100vw';
+}
+
+function closeNav(id){
+    let nav = document.getElementById(id);
+    nav.style.width = '0vw';
+}
